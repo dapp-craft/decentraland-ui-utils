3842a561f2aa94019bb045f45588e3a273bfe9c6
v.skoblik <v.skoblik@m-games-ltd.com>
2023-04-05 16:31:09 +0300

    #52513 #add #package - demo examples

--- demo/src/ui.tsx
+++ demo/src/ui.tsx
@@ -3,10 +3,24 @@ import {ReactEcsRenderer} from '@dcl/sdk/react-ecs'
 import * as ui from '@dcl/ui-scene-utils'
 
 const announcement = new ui.Announcement({value: 'Text center'})
+const loadingIcon = new ui.LoadingIcon()
+const smallIcon = new ui.SmallIcon({image: 'images/icons/heart-icon.png', xOffset: -100, yOffset: 100})
+const mediumIcon = new ui.MediumIcon({image: 'images/icons/heart-icon.png', xOffset: -200, yOffset: 200})
+const largeIcon = new ui.LargeIcon({image: 'images/icons/heart-icon.png', xOffset: -300, yOffset: 300})
+const uiCounter = new ui.UICounter({value: 123})
+const cornerLabel = new ui.CornerLabel({value: 'Label', xOffset: -300, yOffset: 70})
+const uiBar = new ui.UIBar({value: .5, xOffset: -500, yOffset: 60})
 
 const uiComponent = () => {
     return [
         announcement.render(),
+        loadingIcon.render(),
+        smallIcon.render(),
+        mediumIcon.render(),
+        largeIcon.render(),
+        uiCounter.render(),
+        cornerLabel.render(),
+        uiBar.render(),
     ]
 }
 
@@ -15,6 +29,13 @@ export function setupUi() {
 }
 
 announcement.show()
+loadingIcon.show()
+smallIcon.show()
+mediumIcon.show()
+largeIcon.show()
+uiCounter.show()
+cornerLabel.show()
+uiBar.show()
 
 // let timer: number = 10
 //
