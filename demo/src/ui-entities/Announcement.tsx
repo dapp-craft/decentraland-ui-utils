324a852aaa538ea47933f35a41acdbceef8853e1
v.skoblik <v.skoblik@m-games-ltd.com>
2023-04-10 12:35:38 +0300

    #52513 #change #demo - announcement start visible

--- demo/src/ui-entities/Announcement.tsx
+++ demo/src/ui-entities/Announcement.tsx
@@ -1,5 +1,3 @@
 import * as ui from '@dcl/ui-scene-utils'
 
-export const announcement = new ui.Announcement({ value: 'Text center', yOffset: 400, duration: 5 })
-
-announcement.show()
+export const announcement = new ui.Announcement({ startHidden: false, value: 'Text center', yOffset: 400, duration: 5 })
